aed57f2308b45d695c075b7c8f8e82e605c24aa3
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2024-11-14 22:15:01 -0600

    test tabla y cambios de posicion

--- UbyTEC/src/app/affiliate/pages/gestion-pedidos/gestion-pedidos.component.ts
+++ UbyTEC/src/app/affiliate/pages/gestion-pedidos/gestion-pedidos.component.ts
@@ -16,12 +16,19 @@ import { HeaderAffiliateComponent } from '../../components/header/header-affilia
 export class GestionPedidosComponent {
   objects: any[] = [];  // Array para almacenar objetos
   displayedColumns: string[] = [];  // Array para almacenar las columnas a mostrar en la tabla
-  constructor(private table_service: TableService, private router:Router, private api: ApiService){}
+  
+  constructor(private table_service: TableService, private router:Router, private api: ApiService){
+    const obj = {num_pedido: 10, nombre: "juan", estado:"sin repartidor", monto_total: 20000, id_repartidor: 47}
+    this.objects.push(obj);
+  }
 
   ngOnInit(): void {
-    let data = this.api.getData('Pedidos/');
+    
     const columns = ['num_pedido', 'nombre', 'estado', 'monto_total', 'id_repartidor'];  // Definir las columnas a mostrar
-
+    this.table_service.showTable(this.objects, columns);
+    this.displayedColumns = this.table_service.displayedColumns;
+    /**
+    let data = this.api.getData('Pedidos/');
     data.subscribe({
       next: res => {
         this.table_service.showTable(res, columns);  // Mostrar la tabla con los datos y columnas definidos
@@ -29,6 +36,7 @@ export class GestionPedidosComponent {
         this.displayedColumns = this.table_service.displayedColumns;  // Obtener columnas a mostrar desde el servicio},  // Imprimir la respuesta en caso de éxito
       },error: err => {console.error(err) }  // Imprimir el error en caso de fallo
     });
+    **/
 
   }
 
