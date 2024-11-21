dcdb70c5ef20a619b278424720390163ca7d5399
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2024-11-21 03:00:00 -0600

    editar productos

--- UbyTEC/src/app/affiliate/pages/gestion-productos/gestion-productos.component.ts
+++ UbyTEC/src/app/affiliate/pages/gestion-productos/gestion-productos.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm, FormBuilder, FormGroup } from '@angular/forms';
 import { ApiService } from '../../../Services/API/api.service';
 import { HeaderAffiliateComponent } from '../../components/header/header-affiliate.component';
 import { NgFor, NgIf } from '@angular/common';
+import { TableService } from '../../../Services/Table/table.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-gestion-productos',
   standalone: true,
-  imports: [FormsModule, HeaderAffiliateComponent, NgFor, NgIf],
+  imports: [FormsModule, HeaderAffiliateComponent, NgFor, NgIf, MatTableModule],
   templateUrl: './gestion-productos.component.html',
   styleUrl: './gestion-productos.component.css'
 })
 export class GestionProductosComponent {
+  @ViewChild('userForm') userForm!: NgForm;
   productAdded: boolean = false;
   catOptions: string[] = ['Pizza', 'Sandwich', 'Sopa', 'Refresco', 'Gaseosa', 'Té', 'Café', 'Bowl', 'Crepa', 'Plato Fuerte', 'Postre', 'Ensalada'];
   selectedFile: File | null = null;
-  constructor(private api: ApiService, private router:Router){}
+  product_id: number = 0;
+
+  activeTab: string = 'crear';
+  editMode: boolean = false;
+
+  products: any[] = [];
+  displayedColumns: string[] = [];
+
+  constructor(private api: ApiService, private router:Router, private table_service: TableService){}
 
   
   onFileSelected(event: Event): void {
@@ -35,22 +46,61 @@ export class GestionProductosComponent {
     }
   }
 
-  // Método para manejar el envío del formulario
-  submit(form: any) {
-    this.productAdded = true;
-    form.id = 0;
-    let cuerpo = JSON.stringify(form);  // Convertir el formulario a una cadena JSON
-    console.log(cuerpo);  // Imprimir el cuerpo del formulario en la consola
-
-    
-    // Hacer una llamada POST a la API para añadir un nuevo producto
-    let output = this.api.postData("Producto", cuerpo);
-
-    // Suscribirse a la respuesta de la llamada API
-    output.subscribe({
-      next: res => { console.log(res) },  // Imprimir la respuesta en caso de éxito
-      error: err => { console.error(err) }  // Imprimir el error en caso de fallo
-    });
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
   }
 
+  // Método para manejar el envío del formulario
+  submit(form: any) {
+    //form.id = this.product_id;
+    let cuerpo = JSON.stringify(form);  // Convertir el formulario a una cadena JSON
+    console.log(cuerpo);
+    if (this.editMode){
+      this.api.putData('Producto', cuerpo).subscribe({
+        next: res => { this.productAdded = true },  // Imprimir la respuesta en caso de éxito
+        error: err => { console.error(err) }  // Imprimir el error en caso de fallo
+      });
+    }else{  
+      this.api.postData("Producto", cuerpo).subscribe({
+        next: res => { this.productAdded = true },  // Imprimir la respuesta en caso de éxito
+        error: err => { console.error(err) }  // Imprimir el error en caso de fallo
+      });
+    }
+    
+  }
+
+  ngOnInit(){
+    const columns = ['id', 'nombre', 'categoria', 'precio'];  // Definir las columnas a mostrar
+      let data = this.api.getData('Producto/');
+      data.subscribe({
+        next: res => {
+          this.table_service.showTable(res, columns);  // Mostrar la tabla con los datos y columnas definidos
+          this.products = this.table_service.objects;  // Obtener objetos de la tabla desde el servicio
+          this.displayedColumns = this.table_service.displayedColumns;  // Obtener columnas a mostrar desde el servicio},  // Imprimir la respuesta en caso de éxito
+        },error: err => {console.error(err) }  // Imprimir el error en caso de fallo
+      });
+  }
+  
+  editProduct(id: number){
+    this.product_id = id;
+    console.log(this.userForm);
+    this.api.getData(`Producto/${this.product_id}`).subscribe({
+      next: (product) => {
+        console.log(product);
+        this.userForm.form.patchValue({
+          id: product.id,
+          nombre: product.nombre,
+          categoria: product.categoria,
+          precio: product.precio
+        });
+
+        this.editMode = true;
+        this.activeTab = 'crear';
+      }
+    })
+  }
+
+  cancelEdit(){
+    this.editMode = false;
+  }
 }
