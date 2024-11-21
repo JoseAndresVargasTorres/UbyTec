5c3b44ffaba1cf7eda3070279f205ed446e2a7eb
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2024-11-21 00:29:39 -0600

    telefonos

--- UbyTEC/src/app/affiliate/pages/solicitud-afiliacion/solicitud-afiliacion.component.ts
+++ UbyTEC/src/app/affiliate/pages/solicitud-afiliacion/solicitud-afiliacion.component.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../../Services/API/api.service';
 import { GestionAdministradorComponent } from '../gestion-administrador/gestion-administrador.component';
 import { NgFor, NgIf } from '@angular/common';
-import { concat} from 'rxjs';
+import { concat } from 'rxjs';
 import { EmailService } from '../../../Services/Email/email.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class SolicitudAfiliacionComponent {
   tipoOptions: string[] = ['Comida Rápida', 'Comida Italiana', 'Comida Mexicano', 'India', 'China', 'Japonesaq'];
   adminAdded = false;
   admin: any = {};
-  admin_phones: string[] = [];
+  admin_phones: number[] = [];
 
   constructor(private api: ApiService, private router:Router, private dialog: MatDialog, private email_service: EmailService){}
 
@@ -36,14 +36,18 @@ export class SolicitudAfiliacionComponent {
     }
   }
 
-  addAdminPhoneToApi(cedula:string, phone: string) {
-    const cuerpo = JSON.stringify({ cedula_Admin: cedula, telefono: phone });
-    return this.api.postData("TelefonoAdmin", cuerpo);
+  addComercioPhones(cedula: string, phones: string[]) {
+    return JSON.stringify({
+      cedula_Comercio: cedula,
+      telefonos: phones
+    });
   }
 
-  addComercioPhoneToApi(cedula:string, phone: string) {
-    const cuerpo = JSON.stringify({ cedula_Comercio: cedula, telefono: phone });
-    return this.api.postData("TelefonoComercio", cuerpo );
+  addAdminPhones(cedula: string, phones: string[]) {
+    return JSON.stringify({
+      cedula_Admin: cedula,
+      telefonos: phones
+    });
   }
 
   trackByIndex(index: number, item: any): number {
@@ -72,14 +76,12 @@ export class SolicitudAfiliacionComponent {
     return password;
   }
   
-  
   sendEmail(nombre: string, correo: string, usuario: string, password: string) {
     const templateParams = {
       to_name: nombre,
       to_email: correo,
       from_name: 'UbyTec',
       message: `Gracias por afiliarte con nosotros. \n Tu usuario es ${usuario}  y tu contraseña es ${password}`,
-      
     };
 
     this.email_service.sendEmail(templateParams);
@@ -91,38 +93,29 @@ export class SolicitudAfiliacionComponent {
     const { cedula_Juridica, nombre, id_Tipo, provincia, canton, distrito, correo, sinpe} = form;
 
     const new_password = this.generatePassword(8);
-    this.sendEmail(nombreAdmin, correo, usuario, new_password);
+    //this.sendEmail(nombreAdmin, correo, usuario, new_password);
 
     const cuerpo_comercio = JSON.stringify({cedula_Juridica, nombre, correo, sinpe, id_Tipo, cedula_Admin: cedula});
     const cuerpo_direccion = JSON.stringify({id_Comercio: cedula_Juridica, provincia, canton, distrito}); 
     const cuerpo_admin = JSON.stringify({cedula, usuario, password: new_password, nombre: nombreAdmin, apellido1, apellido2})
-    const cuerpo_direccion_admin = JSON.stringify({id_Admin: cedula, provincia: provinciaAdmin, canton: cantonAdmin, distrito: distritoAdmin})
-    
-    /**
-    console.log("Comercio " + cuerpo_comercio);
-    console.log("Direccion Comercio " + cuerpo_direccion);
-    console.log("Administrador " + cuerpo_admin);
-    console.log("Direccion Administrador " + cuerpo_direccion_admin);
-    */
+    const cuerpo_direccion_admin = JSON.stringify({id_Admin: cedula, provincia: provinciaAdmin, canton: cantonAdmin, distrito: distritoAdmin});
+    const cuerpo_telefonos = JSON.stringify(this.phones.map(phone => ({cedula_Comercio: cedula_Juridica, telefono: phone})));
+    const cuerpo_telefonos_admin = JSON.stringify(this.phones.map(phone => ({ cedula_Admin: cedula, telefono: phone})));
 
-    const phoneCall1 = this.phones.map(tel => this.addComercioPhoneToApi(cedula_Juridica, tel));
-    const phoneCall2 = this.admin_phones.map(tel => this.addAdminPhoneToApi(cedula, tel));
 
     const apiCalls = [this.api.postData("Administrador", cuerpo_admin),
                       this.api.postData("DireccionAdministrador", cuerpo_direccion_admin),
                       this.api.postData("ComercioAfiliado", cuerpo_comercio),
                       this.api.postData("DireccionComercio",cuerpo_direccion),
-                      ...phoneCall1,
-                      ...phoneCall2]
+                      this.api.postData("TelefonoComercio", cuerpo_telefonos),
+                      this.api.postData("TelefonoAdmin", cuerpo_telefonos_admin)]
 
     concat(...apiCalls).subscribe({
       next: res => { 
-        console.log(res); // Imprimir la respuesta en caso de éxito
+        console.log(res);
         this.router.navigate(['']);
-      },  
-      error: err => { console.error(err) }  // Imprimir el error en caso de fallo
+      },
+      error: err => { console.error(err) }
     });
-
-
   }
 }
